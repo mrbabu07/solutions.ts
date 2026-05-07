6affa93f388682f4121ac34e489a874f7c451db5
mrbabu07 <mdjahedulislamjaved@gmail.com>
2026-05-07 17:15:59 +0600

    solving

--- solutions.ts
+++ solutions.ts
@@ -32,3 +32,73 @@ function checkType(value: StringOrNumber): string {
 
 console.log(checkType('Hello'))
 console.log(checkType(42));
+
+//problem: 4
+
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+    return obj[key];
+}
+const user = {id: 1, name: "John Doe", age: 21};
+const userName = getProperty(user, 'name');
+console.log(userName);
+
+//problem: 5
+interface Book {
+    title: string;
+    author: string;
+    publishedYear: number;
+}
+
+function toggleReadStatus(book: Book): Book & { isRead: boolean} {
+    return {
+        ...book,
+        isRead: true
+    };
+}
+
+const myBook: Book = {title: "TypeScript Guide",
+    author: "Jane Doe",
+    publishedYear: 2024
+};
+
+const resultBook = toggleReadStatus(myBook);
+console.log(resultBook);
+
+//problem: 6
+
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Student extends Person {
+    grade: string;
+
+    constructor(name: string, age: number, grade: string){
+        super(name, age);
+        this.grade = grade;
+
+    }
+
+    getDetails(): string {
+        return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+    }
+}
+
+const student = new Student("Alice", 20, "A");
+student.getDetails();
+console.log(student.getDetails());
+
+//problem: 7
+
+function getIntersection(arr1: number[], arr2: number[]): number[]{
+    return arr1.filter((num) => arr2.includes(num));
+}
+
+const intersection = getIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]);
+console.log(intersection);
